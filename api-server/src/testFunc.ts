7793297d1ce161957f6b0f9a52e76caeb6e9d3ee
Samuel Lee <lee.samuel@ai2-jp.com>
2022-03-03 14:58:30 +0900

    checking type error when testing typescript files

---
+++ api-server/src/testFunc.ts
@@ -0,0 +1,8 @@
+function getLength(params: string) {
+  return params.length
+}
+
+const greeting = 'hello world'
+const greetingLength = getLength(greeting)
+
+console.log(getLength(greetingLength))
